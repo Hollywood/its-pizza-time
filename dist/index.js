7c76e2c3a1b961b9ff323d5d38e87e7b07569f84
Jonathan Cardona <hollywood@github.com>
2021-02-25 10:15:51 -0500

    updating dist

--- dist/index.js
+++ dist/index.js
@@ -32,11 +32,12 @@ run();
 const fs = __nccwpck_require__(5747)
 const core = __nccwpck_require__(2186)
 const pizzapi = __nccwpck_require__(5636)
-a
+
 async function orderPizza(){
     try {
-        var customerData = fs.readFileSync('./customer.json')
-        var customer = JSON.parse(customerData)
+        var customer = JSON.parse(core.getInput('RECEIVING_ADDRESS', {
+            required: true
+        }))
         var getAddress = new pizzapi.Address(customer.address).getAddressLines()
 
         var closestStoreID = await pizzapi.Util.findNearbyStores(
